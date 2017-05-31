3811cde0ae73303f4b86960dae4c90a84a92add3
manhof <manhof@users.noreply.github.com>
2017-05-31 11:04:49 -0400

    Update cockroach_startup.sh

--- cockroach_startup.sh
+++ cockroach_startup.sh
@@ -11,10 +11,11 @@ store=$7
 master=$8 #This value doesn't matter if this is the master node
 if [[ "$masterb" =  True ]]
  then
-	echo "setting up server as Master" >> /home/test
+    echo "setting up server as Master" >> /home/test
     cockroach start --insecure --advertise-host=$pub --background --http-port=80 --store=/mnt/cockroach --locality=country=$country,state=$state,city=$city,region=$region 
 else
     echo "setting up Server as slave. Connecting to Master $master" >> /home/test
-    cockroach start --insecure --advertise-host=$pub --background --http-port=80 --store=/mnt/cockroach --locality=country=$country,state=$state,city=$city,region=$region --join=$master
+    echo "cockroach start --insecure --advertise-host=$pub --background --http-port=80 --store=/mnt/cockroach --locality=country=$country,state=$state,city=$city,region=$region --join=$master" >> /home/test
+    cockroach start --insecure --advertise-host=$pub --background --http-port=80 --store=/mnt/cockroach --locality=country=$country,state=$state,city=$city,region=$region --join=$master   
 fi
 echo "Cockroachdb has been started and can be accessed at http://$pub" >> /home/test
