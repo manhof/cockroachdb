72ac9024a9527b6b6a297161bd2a59ffdb5d80a7
manhof <manhof@users.noreply.github.com>
2017-06-19 13:06:58 -0400

    Added Support for Secure Clustering
    Added the additional code to allow for cockroachdb to be started in a secure mode

--- cockroach_startup.sh
+++ cockroach_startup.sh
@@ -9,14 +9,42 @@ city=$5
 region=$6
 store=$7
 master=$8 #This value doesn't matter if this is the master node
+secure=$9
+ca_dir=$10
+key_dir=$11
+ca_crt=$12
+crt_crt=$13
+key_key=$14
 echo $master >> /home/test
 if [[ "$masterb" =  True ]]
  then
-    echo "setting up server as Master" >> /home/test
-    cockroach start --insecure --advertise-host=$pub --background --http-port=80 --store=$store --locality=country=$country,state="$state",city="$city",region=$region 
+  if [["$secure" = True ]]
+   then
+    echo "creating CA key pair" >> /home/test
+    cockroach cert create-ca --certs-dir=$ca_dir --ca-key=$key_dir/ca.key
+    echo "Creating a client key pair for the root user" >> /home/test
+    cockroach cert create-client root --certs-dir=$ca_dir --ca-key=$key_dir/ca.key
+    echo "setting up server as Master in Secure Mode" >> /home/test
+    cockroach start --certs-dir=$ca_dir --advertise-host=$pub --background --http-port=443 --store=$store --locality=country=$country,state="$state",city="$city",region=$region
+    echo "Cockroachdb has been started and can be accessed at https://$pub" >> /home/test
+  else
+   echo "setting up server as Master in Insecure Mode" >> /home/test
+   cockroach start --insecure --advertise-host=$pub --background --http-port=80 --store=$store --locality=country=$country,state="$state",city="$city",region=$region 
+   echo "Cockroachdb has been started and can be accessed at http://$pub" >> /home/test
 else
-    echo "setting up Server as slave. Connecting to Master $master" >> /home/test
-    echo "cockroach start --insecure --advertise-host=$pub --background --http-port=80 --store=$store --locality=country=$country,state=$state,city=$city,region=$region --join=$master" >> /home/test
-    cockroach start --insecure --advertise-host=$pub --background --http-port=80 --store=$store --locality=country=$country,state="$state",city="$city",region=$region --join=$master  --logtostderr 
+  if [["$secure" = True ]]
+   then
+    echo "uploading CA key pair" >> /home/test
+    echo $ca_crt >> $ca_dir/ca.crt
+    echo "uploading node crt pair" >> /home/test
+    echo $crt_crt >> $ca_dir/node.crt
+    echo $key_key >> $ca_dir/node.key
+    echo "setting up server as Slave in Secure Mode" >> /home/test
+    cockroach start --certs-dir=$ca_dir --advertise-host=$pub --background --http-port=443 --store=$store --locality=country=$country,state="$state",city="$city",region=$region --join=$master
+    echo "Cockroachdb has been started and can be accessed at https://$pub" >> /home/test
+  else
+   echo "setting up Server as slave (insecure). Connecting to Master $master" >> /home/test
+   echo "cockroach start --insecure --advertise-host=$pub --background --http-port=80 --store=$store --locality=country=$country,state=$state,city=$city,region=$region --join=$master" >> /home/test
+   cockroach start --insecure --advertise-host=$pub --background --http-port=80 --store=$store --locality=country=$country,state="$state",city="$city",region=$region --join=$master  --logtostderr 
+   echo "Cockroachdb has been started and can be accessed at http://$pub" >> /home/test
 fi
-echo "Cockroachdb has been started and can be accessed at http://$pub" >> /home/test
